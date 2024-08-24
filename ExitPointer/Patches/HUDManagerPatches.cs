106c82e3645f05cabb836ef74c8cb4892efd34b8
overfirec <overfirec@outlook.com>
2024-08-25 01:15:59 +0800

    Patches for HUDManager.

---
+++ ExitPointer/Patches/HUDManagerPatches.cs
@@ -0,0 +1,66 @@
+using System;
+using BepInEx.Logging;
+using ExitPointer.Utils;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace ExitPointer.Patches;
+
+[HarmonyPatch(typeof(HUDManager))]
+public class HUDManagerPatches
+{
+    private static ExitPointerClass? _mainExitPointer;
+    private static ExitPointerClass? _fireExitPointer;
+        
+    [HarmonyPatch("Start"), HarmonyPostfix]
+    public static void StartPostfix(ref HUDManager __instance)
+    {
+        Tuple<Texture2D, Texture2D> pointersTextures = ExitPointerPluginRegister.PointerTextures;
+        
+        Tuple<GameObject, GameObject> pointersGameObjects = Tuple.Create(
+            ResourcesLoader.SetGameObject("MainExitPointer", pointersTextures.Item1),
+            ResourcesLoader.SetGameObject("FireExitPointer", pointersTextures.Item2));
+        
+        try
+        {
+            _mainExitPointer = new ExitPointerClass(pointersGameObjects.Item1);
+            _fireExitPointer = new ExitPointerClass(pointersGameObjects.Item2);
+        }
+        catch (Exception e)
+        {
+            ExitPointerPluginRegister.Log($"Error while creating ExitPointers: {e.Message}", LogLevel.Error);
+        }
+
+        
+        _mainExitPointer.SetActive(false);
+        _fireExitPointer.SetActive(false);
+
+        _mainExitPointer.SetDefaultStatus(((Component)__instance.loadingDarkenScreen).transform, new Vector3(0.28f, 0.3f, 1f));
+        _fireExitPointer.SetDefaultStatus(((Component)__instance.loadingDarkenScreen).transform, new Vector3(0.28f, 0.3f, 1f));
+    }
+
+    [HarmonyPatch("Update"), HarmonyPostfix]
+    public static void UpdatePostfix(ref HUDManager __instance)
+    {
+        if (RoundManagerPatches.IsAlready)
+        {
+            if (StartOfRound.Instance.localPlayerController.isInsideFactory)
+            {
+                _mainExitPointer?.SetActive(true);
+                _fireExitPointer?.SetActive(true);
+                
+                _mainExitPointer.MovePointer(RoundManagerPatches.PlayerCamera, RoundManagerPatches.Entrances.Item1);
+                _fireExitPointer.MovePointer(RoundManagerPatches.PlayerCamera, RoundManagerPatches.Entrances.Item2);
+                
+                ExitPointerPluginRegister.Log("ExitPointers are active", LogLevel.Info);
+            }
+            
+            else
+            {
+                _mainExitPointer?.SetActive(false);
+                _fireExitPointer?.SetActive(false);
+            }
+        }
+    }
+}
