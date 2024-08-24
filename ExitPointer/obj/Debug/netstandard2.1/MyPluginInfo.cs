b3706e0ff8fc7b3cec6fa93bbeb2c2e3e0698978
overfirec <overfirec@outlook.com>
2024-08-25 01:16:32 +0800

    Basic Plugin information

---
+++ ExitPointer/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ExitPointer
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "ExitPointer";
+        public const string PLUGIN_NAME = "ExitPointer";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
